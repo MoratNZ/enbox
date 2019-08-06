ab2ed3d76c73d87b352db093cc7970707ecdd745
David Maclagan <morat@Oracle.local>
2019-08-06 21:32:37 +1200

    Add .create method to enbox.lib.config to create config file

--- enbox/lib/config.py
+++ enbox/lib/config.py
@@ -8,7 +8,7 @@ def load(fileName):
     """Load the enbox config file from the specified location, and ensure that it has sane contents.
 
     Arguments:
-        fileName {string} -- the file 
+        fileName {string} -- the file
 
     Returns:
         configParser  -- effectively a dict of dicts of config settings.
@@ -25,3 +25,42 @@ def load(fileName):
     config.read(fileName)
 
     return config
+
+
+def create(fileName, url=None, token="your.token.here"):
+    """Creates a default config file in the indicated location.
+
+    Arguments:
+        fileName {string} -- Path and filename for where you want the file created
+
+    Keyword Arguments:
+        url {string} -- The URL (including http: / https:, but not /api) for your NetBox API (default: {"your.url.here"})
+        token {string} -- An API access token for your enbox user within NetBox (default: {"your.token.here"})
+
+    Returns:
+        string -- Success message
+    """
+    config = configparser.ConfigParser(allow_no_value=True)
+
+    config['NetBox'] = {}
+    config.set(
+        "NetBox", "\n# The URL for the API root for your NetBox install\nUrl", url)
+    config.set("NetBox", "Token", token)
+    config.set("NetBox", "KeyFile", "secrets/keyFile")
+
+    config['ssh'] = {}
+    config.set(
+        "ssh", "\n# Default username to use for SSH connections\nuserName", "sshUserName")
+
+    passwordComment = ("\n# Uncomment the below line if you want to specify an SSH password for the\n"
+                       "# above user.\n"
+                       "#\n"
+                       "# If not specified here, you will be prompted to enter it\n"
+                       "# password = superSekretP455w0rd")
+
+    config.set("ssh", passwordComment)
+
+    with open(fileName, 'w') as configfile:
+        config.write(configfile)
+
+    return "Config file successfully generated"
